463a6fe4394b82d3e8d4a4590886de1348fa34aa
Vasiliy Litvinov <kvas.omsk@gmail.com>
2016-12-13 23:50:09 +0700

    add newline

--- include/pekalib.hpp
+++ include/pekalib.hpp
@@ -2,9 +2,13 @@
 #define PEKALIB_HPP_
 
 /**
-   @brief this func sums two numbers
-   @param a 1st number
-   @param b 2nd number
+  @file pekalib.hpp
+ */
+
+/**
+  @brief this func sums two numbers
+  @param a 1st number
+  @param b 2nd number
  */
 int sum(int a,int b);
 
