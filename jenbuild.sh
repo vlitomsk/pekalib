09cfce8a702edad6a1029795e8d8425899ba43c3
Vasiliy Litvinov <kvas.omsk@gmail.com>
2016-12-13 15:28:06 +0700

    add jenkins build script

---
+++ jenbuild.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -rf build
+mkdir build
+cd build
+cmake .. -DGCOV_COVERAGE=y -DCMAKE_BUILD_TYPE=Debug
+make -j2
+
+lcov --zerocounters --directory .
+lcov --capture --initial --directory . --output-file app
+./tests/utest --gtest_output="xml:utest_testreport.xml"
+lcov --no-checksum --directory . --capture --output-file app.info
+mkdir -p lcov_html
+genhtml -o lcov_html app.info
+
+cd ../doc
+doxygen ./Doxyfile
